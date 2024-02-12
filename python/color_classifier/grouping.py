108fc631c68f8c8fc13544e3a0f6738533cf3d4f
paabram <patrick.w.aabram@gmail.com>
2024-02-12 14:57:58 -0500

    worked on grouping colors into smaller clusters

--- python/color_classifier/grouping.py
+++ python/color_classifier/grouping.py
@@ -1,6 +1,9 @@
 # import matplotlib as mpl 
 import matplotlib.pyplot as plt 
 import pandas as pd 
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 # create dataframe
 colors = pd.read_csv('color_names.csv')
@@ -9,12 +12,34 @@ colors.drop(columns=['Hex (24 bit)', 'Hue (degrees)', 'HSL.S (%)','HSL.L (%), HS
 # rename relevant columns for usability
 colors.rename(columns={'Red (8 bit)': 'Red', 'Green (8 bit)': 'Green', 'Blue (8 bit)':'Blue'}, inplace=True)
 
-fig = plt.figure()
-axes = fig.add_subplot(projection='3d')
+# fit, trans = train_test_split(colors[['Red', 'Green', 'Blue']], random_state=42)
 
-axes.scatter(colors.Red, colors.Green, colors.Blue, c=colors[['Red', 'Green', 'Blue']]/255)
-axes.set_xlabel('R')
-axes.set_ylabel('G')
-axes.set_zlabel('B')
+N_COLORS = 64
 
-plt.show()
+kmeans = KMeans(n_clusters=N_COLORS, random_state=0)
+kmeans.fit(colors[['Red', 'Green', 'Blue']])
+labels = kmeans.labels_
+centers = kmeans.cluster_centers_
+
+colors['Label'] = labels
+colors.to_csv('color_names_clustered.csv')
+
+clusters = pd.DataFrame({'Label': range(centers.shape[0]), 'Red': centers[:,0], 'Green': centers[:,1], 'Blue': centers[:,2]})
+clusters.set_index('Label', inplace=True)
+clusters.to_csv('cluster_centers.csv')
+
+# fig = plt.figure()
+# og = fig.add_subplot(1, 2, 1, projection='3d', azim=48)
+# cl = fig.add_subplot(1, 2, 2, projection='3d', azim=48)
+
+# og.scatter(colors.Red, colors.Green, colors.Blue, c=colors[['Red', 'Green', 'Blue']]/255)
+# og.set_xlabel('R')
+# og.set_ylabel('G')
+# og.set_zlabel('B')
+
+# cl.scatter(colors.Red, colors.Green, colors.Blue, c=[centers[i]/255 for i in labels])
+# cl.set_xlabel('R')
+# cl.set_ylabel('G')
+# cl.set_zlabel('B')
+
+# plt.show()
