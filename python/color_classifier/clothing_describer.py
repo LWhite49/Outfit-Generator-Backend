00cb78149a2a4fa0cf3d1cdffebefab234537637
paabram <patrick.w.aabram@gmail.com>
2024-03-01 11:38:04 -0500

    worked on clothing describer, wrapped in a function

--- python/color_classifier/clothing_describer.py
+++ python/color_classifier/clothing_describer.py
@@ -1,62 +1,56 @@
 from color_model import ColorPredictor
 import cv2 as cv
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 import urllib.request
 import rembg
 import time
 
 from grouping.n_groups import n_groups
 
-# get the image data from the url
-# response = urllib.request.urlopen('https://media-assets.grailed.com/prd/listing/temp/291dabc7cb384d4ab157bfecd69ec027?h=1400&fit=clip&q=20&auto=format')
-response = urllib.request.urlopen('https://media-photos.depop.com/b1/29235888/1777365855_70ea0ea843e24453abbc993578db119c/P0.jpg')
-# response = urllib.request.urlopen('https://media-assets.grailed.com/prd/listing/47158833/2c4899ca3d8044b19ef3504ea97d3c96?w=1050&h=1400&fit=clip&q=20&auto=format')
-# response = urllib.request.urlopen('https://media-assets.grailed.com/prd/listing/temp/d04aba8a74604d58a0cae9d729706daf?h=1400&fit=clip&q=20&auto=format')
-img = cv.imdecode(np.asarray(bytearray(response.read())), cv.IMREAD_COLOR)
-response.close()
-
-# reduce the image size 
+n_groups = n_groups()
 IM_HEIGHT, IM_WIDTH = 200, 200
-img = cv.resize(img, (IM_HEIGHT, IM_WIDTH))
-# get the edge image
-# edges = cv.Canny(img, 150, 200)
 
-# img = cv.bilateralFilter(img, 10, 200, 30)
+def get_colors(url: str) -> list[list[int]]:
+    '''Takes in an image url and returns its predominant color groups in order. Failures will return -1.'''
+    # get the image data from the url
+    response = urllib.request.urlopen(url)
+    img = cv.imdecode(np.asarray(bytearray(response.read())), cv.IMREAD_COLOR)
+    response.close()
 
-# img = img[IM_WIDTH//3:(IM_WIDTH*2)//3, IM_HEIGHT//3:(IM_HEIGHT*2)//3]
+    # reduce the image size 
+    img = cv.resize(img, (IM_HEIGHT, IM_WIDTH))
 
-# change image to use rgb rather than bgr color space
-img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    # change image to use rgb rather than bgr color space
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-# remove background
-s = rembg.new_session('u2netp')
-img = rembg.remove(img, session=s, bgcolor=(0,0,0,0))
+    # blur the image
+    # img = cv.bilateralFilter(img, 10, 500, 25)
 
-cv.imshow('shirt', img)
-cv.waitKey()
+    # remove background
+    s = rembg.new_session('u2netp')
+    img = rembg.remove(img, session=s, bgcolor=(0,0,0,0))
 
-# flatten the image rows and columns
-img = np.reshape(img, (IM_HEIGHT*IM_WIDTH, -1))
-# find all pixels where the color alpha value is 0 and remove them
-drop = []
-for i in range(IM_HEIGHT*IM_WIDTH):
-    if img[i][3] == 0:
-        drop.append(i)
-img = np.delete(img, drop, axis=0)
-# drop alpha column from colors
-img = img[:, :3]
+    # flatten the image rows and columns
+    img = np.reshape(img, (IM_HEIGHT*IM_WIDTH, -1))
+    # find all pixels where the color alpha value is 0 and remove them
+    img = np.delete(img, [i for i in range(IM_HEIGHT*IM_WIDTH) if img[i][3] == 0], axis=0)
+    # drop alpha column from colors
+    img = img[:, :3]
 
-m = ColorPredictor()
-labels = m.predict(img)
+    # get color groups from image
+    m = ColorPredictor()
+    labels = m.predict(img)
 
-votes = [0] * n_groups()
-for i in range(len(labels)):
-    votes[labels[i]] += 1
+    # count pixels of each color
+    votes = [0] * n_groups
+    for i in range(len(labels)):
+        votes[labels[i]] += 1
 
-prevalent = [[x,i] for i, x in enumerate(votes) if x > 50]
+    # order groups by pixel count
+    prevalent = sorted([[x,i] for i, x in enumerate(votes) if x > 50], reverse=True)
 
-print(sorted(prevalent, reverse=True))
+    # return just groups
+    return [x[1] for x in prevalent]
 
 # print(m.predict(img[0][0]))
 # print(img[0][0])
@@ -70,3 +64,9 @@ print(sorted(prevalent, reverse=True))
 # print(votes)
 # for color in votes.keys():
 #     print(votes[color] / max(votes.values()))
+
+# response = urllib.request.urlopen('https://media-photos.depop.com/b1/34570605/1777087976_f762224765eb4cbcb728b9f58ba11978/P0.jpg')
+    # response = urllib.request.urlopen('https://media-photos.depop.com/b1/29235888/1777365855_70ea0ea843e24453abbc993578db119c/P0.jpg')
+    # response = urllib.request.urlopen('https://media-photos.depop.com/b1/44686826/1775473394_0a2ebcb4c0cd413eb05559f51cddbc4e/P0.jpg')
+if __name__ == '__main__':
+    print(get_colors('https://media-photos.depop.com/b1/29235888/1777365855_70ea0ea843e24453abbc993578db119c/P0.jpg'))
