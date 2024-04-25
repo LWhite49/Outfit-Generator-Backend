0957708137bec79412538f7105fb7e9fa1d98383
Ynot400 <147223483+Ynot400@users.noreply.github.com>
2024-04-25 16:15:12 -0400

    Prompt automation
    Figuring out a way to automate our prompting for complementary, similarity, and neutrality

--- python/recommendation_system/data_generator.py
+++ python/recommendation_system/data_generator.py
@@ -12,6 +12,51 @@ import pandas as pd
 import keyboard
 import threading
 from color_assignment.grouping.conversions import hex_to_rgb
+import numpy as np
+
+
+
+def comp_sim_neut_probability(colors1, colors2):
+    complementary_score = 0
+    similar_score = 0
+    neutral_score = 0
+    # Iterate over each subset array in the first array
+    for subset1 in colors1:
+        rgb_value1 = hex_to_rgb(subset1[2])  # Get the hex value from the first array
+        for subset2 in colors2:
+            rgb_value2 = hex_to_rgb(subset2[2])  # Get the hex value from the second array
+
+            # Calculate the probability that two rgb colors are complementary
+            distance = np.linalg.norm((np.array(rgb_value1) + np.array(rgb_value2)) - 255)
+            complementary_prob = 1 - (distance / np.sqrt(3 * (255 ** 2)))
+
+            # Calulate the similarity between the two colors
+            cos_sim = np.dot(rgb_value1, rgb_value2) / (np.linalg.norm(rgb_value1) * np.linalg.norm(rgb_value2))
+
+            # Calculate the neutral probability
+            neutral_prob = 1 - (np.linalg.norm(rgb_value1) - np.linalg.norm(rgb_value2)) / np.linalg.norm(rgb_value1)
+
+            # Compare hex values
+            complementary_score += 0.625 * complementary_prob
+            similar_score += 0.625 * cos_sim
+            neutral_score += 0.625 * neutral_prob
+
+    return complementary_score, similar_score, neutral_score
+
+    
+def calm_lil_color_weighting(complementary_score, similar_score, neutral_score, user_score):
+    # Calculate the weight of the colors
+    complementary_weight = 0.25
+    similar_weight = 0.25
+    neutral_weight = 0.1
+    user_score_weight = 0.4
+
+    # Calculate the weighted sum of the scores
+    weighted_sum = complementary_score * complementary_weight + similar_score * similar_weight + neutral_score * neutral_weight + user_score * user_score_weight
+
+    return round(weighted_sum)
+                
+
 
 # initialize and source env
 load_dotenv(os.path.dirname(SCRIPT_DIR) + '\color_assignment\.env')
@@ -53,6 +98,10 @@ while True:
     while not colors2 or len(colors2) < 4:
         colors2 = random_item()
     
+    # grab the probabilities of the colors
+    # complimentary_score, similar_score, neutral_score = comp_sim_neut_probability(colors1, colors2)
+
+
     # create an image which will be a 1000*1000 matrix of the image colors, proportioned according to their percentage
     # simultaneously build the row to add to the dataframe
     row = []
@@ -84,10 +133,13 @@ while True:
         break
     elif key.is_keypad:
         if key.name == '0':
+            # aesthetic_score = calm_lil_color_weighting(complimentary_score, similar_score, neutral_score, 10)
             df_row.append(10)
         else:
             df_row.append(int(key.name))
-
+        #     aesthetic_score = calm_lil_color_weighting(complimentary_score, similar_score, neutral_score, int(key.name))
+        # df_row.append(aesthetic_score)    
+    
     
     training_data = pd.concat([pd.DataFrame([df_row], columns=training_data.columns), training_data], ignore_index=1)
 
