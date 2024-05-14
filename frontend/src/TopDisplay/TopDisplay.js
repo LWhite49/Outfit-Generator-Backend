dee603a2cd5ead4382fad6b1673f21192569350e
Logan White <135063791+LWhite49@users.noreply.github.com>
2024-05-14 14:30:58 -0400

    TopDisplay Progress - Only need to accomodate brand

--- frontend/src/TopDisplay/TopDisplay.js
+++ frontend/src/TopDisplay/TopDisplay.js
@@ -7,12 +7,15 @@ export const TopDisplay = (props) => {
     return (
         <div className="TopDisplay">
             <div className="Color-Pallet">
-                <div className="ColorDisplay" style={{backgroundColor: (topItem.productColors.length > 0 ? `#${topItem.productColors[0][2]}` : "red") }}></div>
-                <div className="ColorDisplay" style={{backgroundColor: (topItem.productColors.length > 1 ? `#${topItem.productColors[1][2]}` : "orange") }}></div>
-                <div className="ColorDisplay" style={{backgroundColor: (topItem.productColors.length > 2 ? `#${topItem.productColors[2][2]}` : "yellow") }}></div>
-                <div className="ColorDisplay" style={{backgroundColor: (topItem.productColors.length > 3 ? `#${topItem.productColors[3][2]}` : "blue") }}></div>
+                <div className="Color-Display-First" style={{backgroundColor: (topItem.productColors.length > 0 ? `#${topItem.productColors[0][2]}` : "red") }}></div>
+                <div className="Color-Display" style={{backgroundColor: (topItem.productColors.length > 1 ? `#${topItem.productColors[1][2]}` : "orange") }}></div>
+                <div className="Color-Display" style={{backgroundColor: (topItem.productColors.length > 2 ? `#${topItem.productColors[2][2]}` : "yellow") }}></div>
+                <div className="Color-Display-Last" style={{backgroundColor: (topItem.productColors.length > 3 ? `#${topItem.productColors[3][2]}` : "blue") }}></div>
             </div>
-            <img className="TopImage" src={topItem.productImg} alt="Top"></img>
+            <img className="TopImage" src={topItem.productImg} alt="Top" onClick={() => {window.open(`${topItem.productListing}`)}}></img>
+            <div className="Top-Display-Size-Container">
+                <p className="Top-Display-Size-Text"> {topItem.productSize}</p>
+            </div>
         </div>
     )
 }
