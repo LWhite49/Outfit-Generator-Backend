5128492cea6b2c6e0ad8d09da677e401e22406be
paabram <patrick.w.aabram@gmail.com>
2024-05-28 15:22:29 -0500

    tried moving a copy of score combination

---
+++ javascript/controllers/score_combination.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+relative_path = os.path.join(os.path.dirname(__file__), '../../python/recommendation_system')
+sys.path.append(os.path.abspath(relative_path))
+
+import json
+import ast
+from color_calculator import outfit_comparison
+
+def score(item1, item2) -> float:
+    _, similarity, _, _ = outfit_comparison(item1, item2)
+    return similarity
+
+try:
+    if len(sys.argv) == 3:
+        colors1 = ast.literal_eval(sys.argv[1])
+        colors2 = ast.literal_eval(sys.argv[2])
+
+        print(json.dumps(str(score(colors1, colors2))))
+
+    elif len(sys.argv) == 4: 
+        colors1 = ast.literal_eval(sys.argv[1])
+        colors2 = ast.literal_eval(sys.argv[2])
+        colors3 = ast.literal_eval(sys.argv[3])
+        
+        score1 = score(colors1, colors2)
+        score2 = score(colors1, colors3)
+        score3 = score(colors2, colors3)
+
+        print(json.dumps(str((score1 + score2 + score3) / 3)))
+
+except:
+    print(json.dumps('-0.5'))
+
+finally:
+    sys.stdout.flush()
