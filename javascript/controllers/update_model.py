9228a537d70ee998614c8856300763750c2952d0
paabram <patrick.w.aabram@gmail.com>
2024-07-31 17:52:44 -0500

    like buttons working

--- javascript/controllers/update_model.py
+++ javascript/controllers/update_model.py
@@ -45,58 +45,74 @@ arcv = client['archive']
 print('Connected to archive database')
 
 # source outfit ids
-top_id = ObjectId(sys.argv[4]) # todo make this work
-bottom_id = ObjectId(sys.argv[5])
-shoe_id = ObjectId(sys.argv[6])
-reaction = sys.argv[7]
+top_id = ObjectId(sys.argv[4][1:len(sys.argv[4]) - 1])
+bottom_id = ObjectId(sys.argv[5][1:len(sys.argv[4]) - 1])
+shoe_id = ObjectId(sys.argv[6][1:len(sys.argv[4]) - 1])
+reaction = int(sys.argv[7])
 
 # pull associated document from clothes database
 #* right now there isn't a way to see what sex collection an item came from so we have to check the id against both
 # top
-sex = 'M' # we will also attach a sex indicator to outfit sets
+sex_vote = 'M' # we will also attach a sex indicator to outfit sets
 collection = db['topmens']
 top = collection.find_one({'_id': top_id}, projection={'_id': False})
  # _id will be excluded from the returned document, as we will rely on new _id when inserting it into the archive
 if not top:
     collection = db['topwomens']
     top = collection.find_one({'_id': top_id}, projection={'_id': False})
-    sex = 'F' # reassign sex
-    # TODO this reassignment ^ is assuming that outfits will be of a consistent sex throughout
+    sex_vote = 'F' # reassign sex
+
+sexes = [sex_vote] # keep an array of the sex of each item, with the more prevalent one being assigned to the set
 
 # insert top into archive
 collection = arcv['tops']
-if collection.find_one(top): # check that this item hasn't already been archived
+# check that this item hasn't already been archived
+existing_item = collection.find_one(top)
+if existing_item:
     print('Top already in archive.')
+    top_arcv_id = existing_item['_id']
 else:
     result = collection.insert_one(top)
     top_arcv_id = result.inserted_id # save new, unique archival _id
 
 # bottom
+sex_vote = 'M' 
 collection = db['bottommens']
 bottom = collection.find_one({'_id': bottom_id}, projection={'_id': False})
 if not bottom:
     collection = db['bottomwomens']
     bottom = collection.find_one({'_id': bottom_id}, projection={'_id': False})
+    sex_vote = 'F'
+
+sexes.append(sex_vote)
 
 # insert bottom into archive
 collection = arcv['bottoms']
-if collection.find_one(bottom):
+existing_item = collection.find_one(bottom)
+if existing_item:
     print('Bottom already in archive.')
+    bottom_arcv_id = existing_item['_id']
 else:
     result = collection.insert_one(bottom)
     bottom_arcv_id = result.inserted_id
 
 # shoe
+sex_vote = 'M'
 collection = db['shoemens']
 shoe = collection.find_one({'_id': shoe_id}, projection={'_id': False})
 if not shoe:
     collection = db['shoewomens']
     shoe = collection.find_one({'_id': shoe_id}, projection={'_id': False})
+    sex_vote = 'F'
+
+sexes.append(sex_vote)
 
 # insert shoe into archive
 collection = arcv['shoes']
-if collection.find_one(shoe):
+existing_item = collection.find_one(shoe)
+if existing_item:
     print('Shoe already in archive.')
+    shoe_arcv_id = existing_item['_id']
 else:
     result = collection.insert_one(shoe)
     shoe_arcv_id = result.inserted_id
@@ -104,7 +120,11 @@ else:
 # todo: if an item wasn't found we can get by just on the colors passed and make an object from that
 
 # create a new document to represent outfit set
+sex = max(set(sexes), key=sexes.count) # get predominant sex association
 outfit = {'sex': sex, 'top_id': top_arcv_id, 'bottom_id': bottom_arcv_id, 'shoe_id': shoe_arcv_id, 'reaction': reaction}
+
+# add it to the database
+# TODO: don't include identical sets?
 collection = arcv['reacted_sets']
 collection.insert_one(outfit)
 
