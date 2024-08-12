d29f479d2beadab770cf786bae3c166d582a8471
paabram <patrick.w.aabram@gmail.com>
2024-08-12 11:25:41 -0500

    dabbbling

---
+++ python/recommendation_system/monte_carlo.py
@@ -0,0 +1,23 @@
+import pickle
+
+file = open('linear_regression.txt', 'rb')
+model = pickle.load(file)
+file.close()
+
+def random_item(collection):
+    '''Returns the color array of a random item selected from the database.'''
+    c = db[collection] 
+    # sample a random item with a nonempty color array
+    pipeline = [{'$match': {'productColors': {'$ne': []}}}, {'$sample': {'size': 1}}]
+    result = list(c.aggregate(pipeline))
+    if result:
+        random_item = result[0]
+        # access and return the image as a np array
+        img_link = random_item['productImg']
+        f = urllib.request.urlopen(img_link)
+        return plt.imread(f)
+    else:
+        print('No items available with color array.')
+        return None
+
+model._pa
