f37af18130859e8428a01542672ec7e5fccefb96
paabram <patrick.w.aabram@gmail.com>
2024-10-10 12:12:13 -0400

    attempts bug fixes

--- javascript/controllers/score_combination.py
+++ javascript/controllers/score_combination.py
@@ -13,24 +13,27 @@ from recommendation_system.model_predict import Predictor
 import random
 
 def index_palettes(tops, bottoms, shoes, n=20):
-    '''Given sets of tops, bottoms, and shoes return n outfits predicted to be liked by the algorithm. This takes the form of an array of 
-    n dictionaries, each with an index from each collection indicating which item from the original set should be in that outfit.'''
+    '''Given sets of tops, bottoms, and shoes return n outfits predicted to be liked by the algorithm. 
+    This takes the form of an array of n dictionaries, each with an index from each collection indicating which item from the original set should be in that outfit.'''
     model = Predictor() # instantiate predictor
 
     # we will reference lists by index to avoid problems with shifting indices
-    top_idxs = list(range(len(tops)))
-    bottom_idxs = list(range(len(bottoms)))
-    shoe_idxs = list(range(len(shoes)))
+    top_idxs = set(range(len(tops)))
+    bottom_idxs = set(range(len(bottoms)))
+    shoe_idxs = set(range(len(shoes)))
 
     indexed_fits = []
     iter_count = 0
     # iterate until we have n outfits, or 100 iterations
     while len(indexed_fits) < n and iter_count < 100:
+        if not top_idxs or not bottom_idxs or not shoe_idxs:
+            break
+
         # select random indexes from the pool
-        top_i = random.choice(top_idxs)
-        bottom_i = random.choice(bottom_idxs)
-        shoe_i = random.choice(shoe_idxs)
-        
+        top_i = random.choice(list(top_idxs))
+        bottom_i = random.choice(list(bottom_idxs))
+        shoe_i = random.choice(list(shoe_idxs))
+
         # check if the algorithm predicts this outfit to be liked
         if model.predict(tops[top_i], bottoms[bottom_i], shoes[shoe_i]) == 1:
             # store items in a dictionary
@@ -45,22 +48,22 @@ def index_palettes(tops, bottoms, shoes, n=20):
         
         iter_count += 1 # count iterations
     
-    #TODO get a new pallette if theres way too few outfits
     # if we did not get enough outfits
     if len(indexed_fits) < n:
-        # create two times the amount of outfits needed, keeping the better half
         filler_fits = []
         for i in range(2 * (n - len(indexed_fits))):
+            if not top_idxs or not bottom_idxs or not shoe_idxs:
+                break
+
             # select random indexes from the pool
-            top_i = random.choice(top_idxs)
-            bottom_i = random.choice(bottom_idxs)
-            shoe_i = random.choice(shoe_idxs)
-            
+            top_i = random.choice(list(top_idxs))
+            bottom_i = random.choice(list(bottom_idxs))
+            shoe_i = random.choice(list(shoe_idxs))
+
             # get regression score of outfit
             score = model.regress(tops[top_i], bottoms[bottom_i], shoes[shoe_i])
             # store items in a dictionary
             fit = {'top': top_i, 'bottom': bottom_i, 'shoe': shoe_i}
-            # this combination will be kept in a list, score first for sorting
             filler_fits.append((score, fit))
 
             # drop indices from pool
@@ -68,11 +71,10 @@ def index_palettes(tops, bottoms, shoes, n=20):
             bottom_idxs.remove(bottom_i)
             shoe_idxs.remove(shoe_i)
 
-        # sort extra outfits by the score they recieved from the regressor
-        filler_fits.sort(reverse=1)
-        # append outfits from this list until full
+        filler_fits.sort(reverse=True)
+
         i = 0
-        while len(indexed_fits) < n:
+        while len(indexed_fits) < n and i < len(filler_fits):
             indexed_fits.append(filler_fits[i][1])
             i += 1
 
